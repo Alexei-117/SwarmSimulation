e55e94097d52229dc87f65afe24fc7200acf3c2c
Alexei-117 <alexei-117@hotmail.com>
2021-04-27 18:23:08 +0200

    Added FPS Counter. Fixed SubScene not loading. Now project can be built into solution.

---
+++ Swarm/Assets/Scripts/SwarmSimulator/GUI/FPSCounter.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+{
+
+    public float timer, refresh, avgFramerate;
+    string display = "{0} FPS";
+    private Text m_Text;
+
+    private void Start()
+    {
+        m_Text = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        float timelapse = Time.smoothDeltaTime;
+        timer = timer <= 0 ? refresh : timer -= timelapse;
+
+        if (timer <= 0) avgFramerate = (int)(1f / timelapse);
+        m_Text.text = string.Format(display, avgFramerate.ToString());
+    }
+}
