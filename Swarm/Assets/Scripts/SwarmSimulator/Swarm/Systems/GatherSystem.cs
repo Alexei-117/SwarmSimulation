73f03428cb484dcd41d61bd1df9fd43a04668040
Alexei Jilinskiy <alexei-117@hotmail.com>
2021-05-12 18:50:07 +0200

    Updated gather system to check that the agent needs to gather, and then we change the size if the agent is inside the aggregation zone

--- Swarm/Assets/Scripts/SwarmSimulator/Swarm/Systems/GatherSystem.cs
+++ Swarm/Assets/Scripts/SwarmSimulator/Swarm/Systems/GatherSystem.cs
@@ -22,14 +22,20 @@ namespace Swarm.Swarm
         {
             NativeArray<float3> lightsPositions = GenericInformation.GetLightTranslations.Reinterpret<float3>();
             NativeArray<float> lightsSizes = GenericInformation.GetLights.Reinterpret<float>();
+            float agentSize = GenericInformation.AgentSize * 0.5f;
 
-            Dependency = Entities.WithAll<AgentTag>().ForEach((ref PotentialValue potentialField, in Translation t, in Gather gather) =>
+            Dependency = Entities.WithAll<AgentTag>().ForEach((ref Gather gather, ref CompositeScale size, in PotentialValue potentialField, in Translation t) =>
             {
+                size.Value = float4x4.Scale(0.75f, 1.0f, 0.75f);
                 for (int i = 0; i < lightsPositions.Length; i++)
                 {
-                    if (math.distance(new float2(t.Value.x, t.Value.z), new float2(lightsPositions[i].x, lightsPositions[i].z)) <= lightsSizes[i])
+                    if (math.distance(new float2(t.Value.x, t.Value.z), new float2(lightsPositions[i].x, lightsPositions[i].z)) <= (lightsSizes[i] + agentSize))
                     {
-                        potentialField.Value += gather.Value;
+                        gather.Value = true;
+                    }
+                    if (math.distance(new float2(t.Value.x, t.Value.z), new float2(lightsPositions[i].x, lightsPositions[i].z)) <= 10.0f)
+                    {
+                        size.Value = float4x4.Scale(0.3f, 2.0f, 0.3f);
                     }
                 }
             }).Schedule(Dependency);
